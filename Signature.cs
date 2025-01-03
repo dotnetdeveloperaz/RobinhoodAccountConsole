4dd6ef235ee3b6b5f42dc26fe94de5c35e371175
DotnetDeveloperAZ <dotnetdeveloperaz@gmail.com>
2025-01-02 19:32:23 -0700

    #3 Added functionality for body in message. Will need to test after implementing other endpoint usage that uses it.

--- Signature.cs
+++ Signature.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Crypto.Signers;
 using Org.BouncyCastle.Crypto;
@@ -15,20 +10,52 @@ internal class Signature
 {
     private string _method;
     private string _path;
+    private string? _body = null;
     private long _timestamp;
     private bool _isSignatureValid = false;
     private ApiServer _apiServer;
     private string? _base64Signature;
+    private bool _hasBody = false;
 
     public long Timestamp { get { return _timestamp; } } 
     public bool IsSignatureValid { get { return _isSignatureValid; } }
     public string? Base64Signature { get { return _base64Signature; } }
+    public string? Body { get { return _body; } }
+    public bool HasBody { get { return _hasBody; } }
+
+    /// <summary>
+    /// Creates the signature object to create the signature for the request without a body
+    /// </summary>
+    /// <param name="apiserver"></param>
+    /// <param name="method"></param>
+    /// <param name="path"></param>
+    /// 
     public Signature(ApiServer apiserver, string method, string path)
     {
         _apiServer = apiserver;
         _method = method;
         _path = path;
     }
+    /// <summary>
+    /// Creates the signature object to create the signature for the request with body
+    /// </summary>
+    /// <param name="apiserver"></param>
+    /// <param name="method"></param>
+    /// <param name="path"></param>
+    /// <param name="body"></param>
+    public Signature(ApiServer apiserver, string method, string path, string body)
+    {
+        _apiServer = apiserver;
+        _method = method;
+        _path = path;
+        _body = body;
+        _hasBody = true;
+    }
+
+    /// <summary>
+    /// Signs the message for the API call.
+    /// </summary>
+    /// <returns></returns>
     internal bool Sign()
     {
         // Convert base64 strings to bytes
@@ -41,10 +68,14 @@ internal class Signature
 
         // Create the message to sign
         _timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        string message = $"{_apiServer.ApiKey}{_timestamp}{_path}{_method}";
+        string message;
+        if (_body != null)
+            message = $"{_apiServer.ApiKey}{_timestamp}{_path}{_method}{_body}";
+        else
+            message = $"{_apiServer.ApiKey}{_timestamp}{_path}{_method}";
 
-        // Sign the message
-        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            // Sign the message
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
         ISigner signer = new Ed25519Signer();
         signer.Init(true, edPrivateKey);
         signer.BlockUpdate(messageBytes, 0, messageBytes.Length);
